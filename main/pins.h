9b07a34cf3a419eeb37b7a7f03ebac19debfe3ab
Felipe Carbonell <fbcarbonell22@gmail.com>
2026-05-21 14:58:16 -0300

    a

--- main/pins.h
+++ main/pins.h
@@ -1,16 +1,16 @@
 #ifndef PINS_H
 #define PINS_H
 
-const int BTN_PIN_R = 4;
-const int BTN_PIN_G = 5;
-const int BTN_PIN_B = 6;
-const int LED_PIN_R = 7;
-const int LED_PIN_G = 8;
-const int LED_PIN_B = 9;
-const int MPU = 15;
-const int FUSION = 11;
-const int UART = 12;
-const int LED_PWM = 13;
+#define BTN_PIN_R  4
+#define BTN_PIN_G  5
+#define BTN_PIN_B  6
+#define LED_PIN_R  7
+#define LED_PIN_G  8
+#define LED_PIN_B  9
+#define MPU        15
+#define FUSION     11
+#define UART       12
+#define LED_PWM    13
 
 
 
